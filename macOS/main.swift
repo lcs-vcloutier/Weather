9e36ce76b0579ad9de78f7e83280aaef258ad14d
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-09-30 09:04:01 -0400

    ask user for input

--- macOS/main.swift
+++ macOS/main.swift
@@ -10,7 +10,18 @@ import Foundation
 // Generate a weather prediction
 let prediction = WeatherPredictionGenerator.getPrediction()
 
+// Program name
+print("WEATHER APP")
+print("============")
+
+// Ask for user's question
+print("Ask what's the weather:")
+// Get input as a non-optional String
+let input = readLine()!
+
+
 // Show the prediction
 print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
 
 print("That's \(prediction.feel.lowercased())!")
+
