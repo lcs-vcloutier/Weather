b4fe6c910c7d2d6f09e05fc35253093007c500e2
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-09-30 09:14:19 -0400

    got query loop going - need to add history

--- Shared/ViewModels/WeatherViewModel.swift
+++ Shared/ViewModels/WeatherViewModel.swift
@@ -1,9 +1,33 @@
+////
+////  WeatherViewModel.swift
+////  Weather
+////
+////  Created by Russell Gordon on 2021-09-30.
+////
 //
-//  WeatherViewModel.swift
-//  Weather
+//import Foundation
 //
-//  Created by Russell Gordon on 2021-09-30.
+//class WeatherViewModel: ObservableObject {
 //
-
-import Foundation
-
+//    // History of advice sessions
+//    @Published var sessions: [Prediction] = []    // empty array to start
+//
+//    // Given a question, provide some advice
+//    // Library shortcut: Command-Shift-L
+//    func provideResponseFor(givenQuery: String) -> String {
+//
+//        // Start an advice session
+//        var currentSession = Prediction(temperature: <#T##Double#>, feel: <#T##String#>, condition: <#T##WeatherCondition#>)
+//
+//        // Use the static function right from the Magic8Ball type (no instance required!)
+//        currentSession.response = WeatherPredictionGenerator.getPrediction()
+//
+//        // Save the question and the advice given to the history
+//        sessions.append(currentSession)
+//
+//        // Give the response back
+//        return currentSession.response
+//
+//    }
+//
+//}
--- macOS/main.swift
+++ macOS/main.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+
+// Make an instance of the view model (to store questions and advice)
+//var advisor = WeatherViewModel()
+
 // Generate a weather prediction
 let prediction = WeatherPredictionGenerator.getPrediction()
 
@@ -14,14 +18,55 @@ let prediction = WeatherPredictionGenerator.getPrediction()
 print("WEATHER APP")
 print("============")
 
-// Ask for user's question
-print("Ask what's the weather:")
-// Get input as a non-optional String
-let input = readLine()!
 
+// Loop until user chooses to quit
+while true {
+    
+    // Ask for user's question
+    print("Enter Query Here:")
+    
+    // Get input as a non-optional String
+    let input = readLine()!
+    print("")
 
-// Show the prediction
-print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
+    // Show the prediction
+    print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
 
-print("That's \(prediction.feel.lowercased())!")
-
+    print("That's \(prediction.feel.lowercased())!")
+    
+    // Ask whether the user wants to continue or see history of advice
+    while true {
+        print("")
+        print("See history of advice given? (Y/N)")
+        let selection = readLine()!
+        if selection == "Y" {
+            
+            // Show history
+            print("")
+            print("History")
+            print("-------")
+//            for session in advisor.sessions {
+//                print(session.question)
+//                print(session.response)
+//                print("")
+//            }
+            break
+            
+        } else if selection == "N" {
+            break
+        }
+    }
+    
+    // Ask whether the user wants to see more advice
+    while true {
+        print("More advice? (Y/N)")
+        let selection = readLine()!
+        if selection == "Y" {
+            break
+        } else if selection == "N" {
+            exit(0)
+        }
+    }
+    
+    
+}
