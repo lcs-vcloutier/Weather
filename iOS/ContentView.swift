4a8f98c7a0e97ef14a31d1922cfe7615b9cdf061
Sofia Andrade <sandrade@lcs.on.ca>
2021-10-01 11:26:08 -0400

    added iOS target

---
+++ iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS
+//
+//  Created by Sofia Andrade on 2021-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
